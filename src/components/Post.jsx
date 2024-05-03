292c1250ed08bb9fbe56b96ace7034995f0a9e77
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-05-03 23:57:49 +0300

    Add a NewPost component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import classes from "./Post.module.css";
 function Post(props) {
   return (
     <li className={classes.post}>
-      <p className={classes.author}>{props.name}</p>
+      <p className={classes.author}>{props.author}</p>
       <p className={classes.text}>{props.body}</p>
     </li>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,29 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
   return (
-    <ul className={classes.posts}>
-      <Post name="Yonatan Dvir" body="Hello! I'm Learning React!" />
-      <Post name="Yonatan" body="Hello!!" />
-    </ul>
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Yonatan" body="Hello!!" />
+      </ul>
+    </>
   );
 }
 
