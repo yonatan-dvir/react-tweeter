095b33091d4fc314d5bb938a360725e66b77ba32
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-05-08 18:08:32 +0300

    Add cancel button to the new post form

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -22,6 +22,7 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       )}
