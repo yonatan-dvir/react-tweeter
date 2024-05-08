35a753ed96aa735d4d2a078ea914e3df46660005
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-05-08 17:42:49 +0300

    Add the feature of closing the new post window when pressing on the background

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,6 +7,7 @@ import Modal from "./Modal";
 function PostList() {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+  const [modalIsVisible, setModalIsVisible] = useState(true);
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -14,14 +15,21 @@ function PostList() {
   function authorChangeHandler(event) {
     setEnteredAuthor(event.target.value);
   }
+  function hideModalHandler(event) {
+    setModalIsVisible(false);
+  }
+
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Yonatan" body="Hello!!" />
